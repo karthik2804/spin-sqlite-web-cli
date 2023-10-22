82d42090825eaf55da9f6c532a7bd9ecc91dcc6c
karthik2804 <karthik.ganeshram@fermyon.com>
2023-10-22 11:40:01 +0530

    redesing to be a cli
    Signed-off-by: karthik2804 <karthik.ganeshram@fermyon.com>

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
 import { HandleRequest, HttpRequest, HttpResponse, Router } from "@fermyon/spin-sdk"
 import {
+  handleAuth,
   returnHtml
 } from "./utils"
-import { execute } from "./sqlite"
+import { dump, execute } from "./sqlite"
 const router = Router()
 
 router.post("/internal/sqlite/execute", async (_, req: HttpRequest) => { return await execute(req) })
+router.get("/internal/sqlite/validateLogin", async (_, req: HttpRequest) => { return await validateLogin(req) })
+router.post("/internal/sqlite/dump", async (_, req: HttpRequest) => { return await dump(req) })
 router.get("*", returnHtml)
+
+async function validateLogin(req: HttpRequest) {
+  if ((await handleAuth(req.headers["authorization"]))) {
+    return {
+      status: 200
+    }
+  }
+  return {
+    status: 401
+  }
+}
 
 export const handleRequest: HandleRequest = async function (request: HttpRequest): Promise<HttpResponse> {
   return await router.handleRequest(request, request)
